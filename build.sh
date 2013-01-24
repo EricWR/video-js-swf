aaf907879af664f1b3dbece6923115da2e0b5445
EricWR <eric@ericwright.org>
2013-01-23 19:00:33 -0500

    Revert build.sh
    Undid previous two commits because they were unnecessary and I don't know how to revert.

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 # Compiles the plugin using the free Flex SDK on Linux/Mac:
 # http://opensource.adobe.com/wiki/display/flexsdk/Flex+SDK
 
-# If compiling to 10.0, may require the 10.0 playerglobal.swc from the 4.1 flex build located here:
-# http://blogs.adobe.com/flex/2012/05/looking-for-old-flex-sdks.html
-
 # From a shell prompt: sh build.sh
 
 echo "Compiling video-js.swf..."
 
 # Make sure the path to mxmlc is correct!
-/Developer/SDKs/flex_sdk_4/bin/mxmlc ./src/VideoJS.as -o ./bin-release/video-js.swf -use-network=false -static-link-runtime-shared-libraries=true -target-player=10.0.0 -swf-version=10
+/Developer/SDKs/flex_sdk_4/bin/mxmlc ./src/VideoJS.as -o ./bin-release/video-js.swf -use-network=false -static-link-runtime-shared-libraries=true
